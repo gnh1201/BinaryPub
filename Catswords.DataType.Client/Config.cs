600a1078611d8978707fe57334f8b07adbcd1b2f
Namhyeon, Go <gnh1201@gmail.com>
2024-04-08 16:05:53 +0900

    Update Config.cs

--- Catswords.DataType.Client/Config.cs
+++ Catswords.DataType.Client/Config.cs
@@ -2,7 +2,7 @@
 {
     public class Config
     {
-        public static string SEARCH_URL = "http://utilhome.cafe24.com/extlist.xml?q=";
+        public static string SEARCH_URL = "https://exts.kr/extlist.xml?q=";
         public static string MASTODON_HOST = "catswords.social";
         public static string MASTODON_ACCESS_TOKEN = "HDVTEfLswvSJZq5MRpim2tp7DifTcgKbMl0mBM5-uHw";
     }
